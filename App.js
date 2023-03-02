a8a6dd72bc50eb336bd1359def51b07dd6473c58
Jishnu <jishnu.dev.roy@gmail.com>
2023-03-02 22:51:02 +0530

    bug in sendSmS

---
+++ App.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import {
+  View, TextInput, Button, Text, Alert, Pressable, Linking,  PermissionsAndroid
+} from 'react-native';
+
+import { styles } from "./styles"
+import SMS from 'react-native-sms';
+
+const sendSMS = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.SEND_SMS,
+      {
+        title: 'SMS Permission',
+        message: 'This app needs access to send SMS',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      SMS.send('+1234567890', 'Hello, World!');
+    } else {
+      console.log('SMS permission denied');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [inputVisible, setInputVisible] = useState(true);
+
+  const handleSubmit = () => {
+    setInputVisible(false);
+  };
+
+  const handleOtherButtonPress = () => {
+    Alert.alert(
+      'Send SMS', //title
+      'Are you sure?',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Ok',
+          onPress: () => {
+            Alert.alert('Ok Pressed')
+            //send sms action here
+          },
+          style: 'ok',
+        },
+      ],
+      {
+        cancelable: true,
+        onDismiss: () =>
+          Alert.alert(
+            'This alert was dismissed by tapping outside of the alert dialog.',
+          ),
+      },
+    );
+
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={{ margin: 8 }}>
+        <Text style={styles.PNRHeader}>{inputVisible ? 'Enter PNR' : "PNR: " + inputValue}</Text>
+        {inputVisible ? (
+          <View style={{ flexDirection: 'row', alignItems: 'center', }}>
+            <TextInput
+              style={styles.PNRInput}
+              onChangeText={setInputValue}
+              value={inputValue}
+              keyboardType="numeric"
+            />
+            <Pressable style={styles.submitButton} onPress={handleSubmit}><Text style={styles.buttonText}>Submit</Text></Pressable>
+          </View>
+        ) : null}
+        {!inputVisible ? (
+          <View >
+            <Button title="Ticket Status" onPress={() => {
+              Linking.openURL(`sms:139?body=PNR ${inputValue}`)
+            }} />
+            <Button title="Reserved Ticket Berth Status" onPress={handleOtherButtonPress} />
+            <Button title="Button 3" onPress={sendSMS} />
+            <Button title="Reset PNR" onPress={() => setInputVisible(true)} />
+          </View>
+        ) : null}
+      </View>
+    </View>
+  );
+};
+
+export default App;
---
+++ styles.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1, justifyContent: 'center', alignItems: 'stretch' 
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        borderRadius: 12,
+        marginHorizontal: 8
+    },
+    title: {
+        fontSize: 32,
+    },
+    PNRHeader: {
+        fontSize: 32,
+    },
+    PNRInput: {
+        height: 40, borderColor: 'gray', borderWidth: 1, marginRight: 10, flex: 3
+    },
+    submitButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'black',
+        flex: 1
+    },
+    buttonText: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+    }
+
+});
